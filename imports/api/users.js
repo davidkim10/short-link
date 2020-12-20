3c0afcb8d92c2921e8408974ecab0555d3175a3e
David K <dkim@thomasnet.com>
2020-12-20 00:41:19 -0500

    Branding assets added

--- imports/api/users.js
+++ imports/api/users.js
@@ -1,17 +1,34 @@
-import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema';
 import { Accounts } from 'meteor/accounts-base';
 
 Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
+  const email = user.emails[0].address;
+  const { firstName, lastName } = user.profile;
 
-    new SimpleSchema({
-        email: {
-            type: String,
-            regEx: SimpleSchema.RegEx.Email
+  new SimpleSchema({
+    email: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Email,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      custom: function () {
+        if (!this.value.length) {
+          return SimpleSchema.ErrorTypes.REQUIRED;
         }
-    }).validate({ email });
+      },
+    },
+    lastName: {
+      type: String,
+      required: true,
+      custom: function () {
+        if (!this.value.length) {
+          return SimpleSchema.ErrorTypes.REQUIRED;
+        }
+      },
+    },
+  }).validate({ email, firstName, lastName });
 
-    console.log('this is the user', user);
-    return true;
+  return true;
 });
