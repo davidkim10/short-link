2d2aa2f91cc037a950fa022d5497cec44bf1d1fb
David K <dkim@thomasnet.com>
2020-12-20 00:42:02 -0500

    Adjustments for updated reg flow

--- server/main.js
+++ server/main.js
@@ -2,15 +2,15 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
-import {Links} from '../imports/api/links';
+import { Links } from '../imports/api/links';
 import '../imports/startup/simple-schema-configuration.js';
 
 Meteor.startup(() => {
   WebApp.connectHandlers.use((req, res, next) => {
     const _id = req.url.slice(1);
-    const link = Links.findOne({_id});
+    const link = Links.findOne({ _id });
 
-    if(link) {
+    if (link) {
       res.statusCode = 302;
       res.setHeader('Location', link.url);
       res.end();
@@ -19,4 +19,4 @@ Meteor.startup(() => {
       next();
     }
   });
-}); 
+});
